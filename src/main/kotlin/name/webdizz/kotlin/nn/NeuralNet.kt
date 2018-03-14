afdedab438afeedfbcbbf17f4dc702f4140f4bfb
Izzet Mustafaiev <izzet@mustafaiev.com>
2018-03-14 17:49:22 +0200

    Complete NN implementation with fixes to make NN to learn

--- src/main/kotlin/name/webdizz/kotlin/nn/NeuralNet.kt
+++ src/main/kotlin/name/webdizz/kotlin/nn/NeuralNet.kt
@@ -4,50 +4,51 @@ import koma.*
 import koma.extensions.*
 import koma.matrix.Matrix
 
-class NeuralNet(private val iterationsNum: Int = 1_000_000, private val printEveryIteration: Int = 50_000) {
+class NeuralNet(private val epochs: Int = 10_000, private val printEvery: Int = 1_000) {
 
-    private var syn0 = 2 * randn(3, 4, 1) - 1
-    private var syn1 = 2 * randn(4, 1, 1) - 1
+    private val seed = setSeed(1)
+    private var syn0 = 2 * rand(3, 4) - 1
+    private var syn1 = 2 * rand(4, 1) - 1
+    private val bias = 0.0
 
-    fun train(X: Matrix<Double>, y: Matrix<Double>) {
-        for (i in 1..iterationsNum) {
+    fun train(x: Matrix<Double>, y: Matrix<Double>) {
+        for (i in 1..epochs) {
             // start network training
-            val l0 = X
+            val l0 = x
 
-            val l1 = sigmoid(l0 * syn0)
-            val l2 = sigmoid(l1 * syn1)
+            val l1 = activate(l0 * syn0 + bias)
+            val l2 = activate(l1 * syn1 + bias)
 
             // resolve errors to backpropagate
             val l2Error = y - l2
-            val l2Delta = l2Error emul sigmoidDx(l2)
+            val l2Delta = l2Error emul activateDx(l2)
 
             //l1 layer
             val l1Error = l2Delta * syn1.T
-            val l1Delta = l1Error * sigmoidDx(l1)
+            val l1Delta = l1Error emul activateDx(l1)
 
             // update weights
             syn1 += l1.T * l2Delta
             syn0 += l0.T * l1Delta
 
-            if (i % printEveryIteration == 0) {
-                println("==>Mean error ${mean(abs(l2Error))} per input:\n${l2Error} on iteration ${i}")
+            if (i % printEvery == 0) {
+                println("==>Error ${mean(abs(l2Error))}")
             }
         }
     }
 
-    fun test(X: Matrix<Double>): Int {
-        val l0 = X
-        val l1 = sigmoid(l0 * syn0)
-        val l2 = sigmoid(l1 * syn1)
-
-        return if (l2.get(0) > 0.5) 1 else 0
-    }
-
-    fun sigmoidDx(x: Matrix<Double>): Matrix<Double> {
+    fun activateDx(x: Matrix<Double>): Matrix<Double> {
         return x emul (1 - x)
     }
 
-    fun sigmoid(x: Matrix<Double>): Matrix<Double> {
+    fun activate(x: Matrix<Double>): Matrix<Double> {
         return epow((1 + exp(-x)), -1.0)
+    }
+
+    fun test(x: Matrix<Double>): Int {
+        val l0 = x
+        val l1 = activate(l0 * syn0 + bias)
+        val l2 = activate(l1 * syn1 + bias)
+        return if (abs(l2.get(0)) > 0.3) 1 else 0
     }
 }
--- src/test/kotlin/name/webdizz/kotlin/nn/NeuralNetTest.kt
+++ src/test/kotlin/name/webdizz/kotlin/nn/NeuralNetTest.kt
@@ -1,6 +1,7 @@
 package name.webdizz.kotlin.nn
 
-import koma.*
+import koma.end
+import koma.mat
 import org.junit.Test
 import kotlin.test.assertEquals
 
@@ -8,17 +9,18 @@ class NeuralNetTest {
 
     @Test
     fun shouldUseNeuralNet() {
-        val X = mat[0, 0, 1 end
+        val x = mat[0, 0, 1 end
                 0, 1, 1 end
                 1, 0, 1 end
                 1, 1, 1]
         val y = mat[0, 1, 1, 0].T
 
         val nn = NeuralNet()
-        nn.train(X, y)
+        nn.train(x, y)
 
         assertEquals(1, nn.test(mat[0, 1, 1]))
         assertEquals(0, nn.test(mat[0, 0, 1]))
+        assertEquals(1, nn.test(mat[1, 0, 1]))
+        assertEquals(0, nn.test(mat[1, 1, 1]))
     }
 }
-

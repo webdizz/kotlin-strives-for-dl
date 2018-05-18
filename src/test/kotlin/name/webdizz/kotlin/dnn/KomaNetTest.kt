e5a1372a4bb0eead29798183e0256ac435b80d79
Izzet Mustafaiev <izzet@mustafaiev.com>
2018-05-18 14:20:19 +0300

    Update input and expected result to 3 input XOR

--- src/test/kotlin/name/webdizz/kotlin/dnn/KomaNetTest.kt
+++ src/test/kotlin/name/webdizz/kotlin/dnn/KomaNetTest.kt
@@ -9,19 +9,18 @@ class KomaNetTest {
 
     @Test
     fun shouldTrainNeuralNet() {
-        val x = mat[0, 0, 1 end
-                0, 1, 1 end
-                1, 0, 1 end
-                1, 1, 1]
+        val x = mat[0, 0, 0 end
+                0, 0, 1 end
+                1, 0, 0 end
+                1, 1, 0]
         val y = mat[0, 1, 1, 0].T
 
         val nn = KomaNet()
         nn.train(x, y)
 
-        assertEquals(1, nn.test(mat[0, 1, 1]))
-        assertEquals(0, nn.test(mat[0, 0, 1]))
-        assertEquals(1, nn.test(mat[1, 0, 1]))
-        assertEquals(0, nn.test(mat[1, 1, 1]))
-        assertEquals(1, nn.test(mat[1, 1, 0]))
+        assertEquals(0, nn.test(mat[0, 1, 1]))
+        assertEquals(1, nn.test(mat[0, 0, 1]))
+        assertEquals(1, nn.test(mat[1, 1, 1]))
+        assertEquals(0, nn.test(mat[1, 1, 0]))
     }
 }
--- src/test/kotlin/name/webdizz/kotlin/dnn/KomputationNetTest.kt
+++ src/test/kotlin/name/webdizz/kotlin/dnn/KomputationNetTest.kt
@@ -10,18 +10,18 @@ class KomputationNetTest {
 
     @Test
     fun shouldTrainNeuralNet() {
-        val x = mat[0, 0, 1 end
-                0, 1, 1 end
-                1, 0, 1 end
-                1, 1, 1]
+        val x = mat[0, 0, 0 end
+                0, 0, 1 end
+                1, 0, 0 end
+                1, 1, 0]
         val y = mat[0, 1, 1, 0]
 
         val nn = KomputationNet()
         nn.train(x, y)
 
-        assertEquals(1, nn.test(mat[0, 1, 1]))
-        assertEquals(0, nn.test(mat[0, 0, 1]))
-        assertEquals(1, nn.test(mat[1, 0, 1]))
-        assertEquals(0, nn.test(mat[1, 1, 1]))
+        assertEquals(0, nn.test(mat[0, 1, 1]))
+        assertEquals(1, nn.test(mat[0, 0, 1]))
+        assertEquals(0, nn.test(mat[1, 1, 0]))
+        assertEquals(1, nn.test(mat[1, 0, 0]))
     }
 }
